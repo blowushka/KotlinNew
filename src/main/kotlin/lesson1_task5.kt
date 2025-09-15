9b4366512bd80c3fdd4703f8354f9a7fff507d59
slowly <slowly@live.ru>
2025-09-15 23:06:33 +0300

    KN-1-5 display flight time in format 02:02:02

---
+++ src/main/kotlin/lesson1_task5.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val seconds = 6480
+
+    val hours = seconds / 3600
+    val remainingAfterHours = seconds % 3600
+    val minutesLeft = remainingAfterHours / 60
+    val secondsLeft = remainingAfterHours % 60
+
+    val hh = "%02d".format(hours)
+    val mm = "%02d".format(minutesLeft)
+    val ss = "%02d".format(secondsLeft)
+
+    println("$hh:$mm:$ss")
+
+}
