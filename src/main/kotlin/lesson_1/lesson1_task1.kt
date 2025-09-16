058a6d2940101292c6e564dc523dcab3e30686a8
slowly <slowly@live.ru>
2025-09-16 12:17:44 +0300

    KS-1-1 refactoring

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,8 +1,8 @@
 package org.example.lesson_1
 
-fun main(){
-    val ordersCount:Int = 75
-    val gratitudeMessage:String = "Thank you for your purchase!"
+fun main() {
+    val ordersCount: Int = 75
+    val gratitudeMessage: String = "Thank you for your purchase!"
 
     println(ordersCount)
     println(gratitudeMessage)
