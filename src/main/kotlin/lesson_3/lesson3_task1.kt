e931cc3d3ea45145357047bfaf9e9745d897dda6
slowly <slowly@live.ru>
2025-09-17 14:52:58 +0300

    KS-3-1 greeting message

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var greetings = "Good day"
+    val userName = "Ilia"
+
+    println("$greetings, $userName!")
+
+    greetings = "Good evening"
+
+    println("$greetings, $userName!")
+
+}
