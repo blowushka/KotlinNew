872ec73bc109c35d742f8bba5e66afcc3e47a565
slowly <slowly@live.ru>
2025-09-17 15:40:23 +0300

    KS-3-2 wedding change of surname

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val surName = "Андреева"
+    val firstName = "Татьяна"
+    val lastName = "Сергеевна"
+    var age = 20
+
+
+    println("$surName $firstName $lastName, $age")
+
+    val weddingName = "Сидорова"
+    age = 22
+
+    println("$weddingName $firstName $lastName, $age")
+
+}
