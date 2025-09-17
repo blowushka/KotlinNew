6b019f9fa916ca65b5ff24cf6df9d9a60d10691d
slowly <slowly@live.ru>
2025-09-17 15:47:02 +0300

    KS-2-3 refactoring constants to proper UPPER_SNAKE_CASE style, move known variables out of constants

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,16 +1,17 @@
 package org.example.lesson_2
 
-const val departureHour = 9
-const val departureMinute = 39
-const val travelTime = 457
-const val minutesAndHoursConverter = 60
-const val dayTimeConverter = 24
+const val MINUTES_AND_HOURS_CONVERTER = 60
+const val DAY_TIME_CONVERTER = 24
 
 fun main() {
 
-    val totalMinutes = departureHour * minutesAndHoursConverter + departureMinute + travelTime
-    val arrivalHour = (totalMinutes / minutesAndHoursConverter) % dayTimeConverter
-    val arrivalMinute = totalMinutes % minutesAndHoursConverter
+    val departureHour = 9
+    val departureMinute = 39
+    val travelTime = 457
+
+    val totalMinutes = departureHour * MINUTES_AND_HOURS_CONVERTER + departureMinute + travelTime
+    val arrivalHour = (totalMinutes / MINUTES_AND_HOURS_CONVERTER) % DAY_TIME_CONVERTER
+    val arrivalMinute = totalMinutes % MINUTES_AND_HOURS_CONVERTER
 
     println("Arrival time: %02d:%02d".format(arrivalHour, arrivalMinute))
 
