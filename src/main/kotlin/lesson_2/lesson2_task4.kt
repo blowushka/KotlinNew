64979a82114f9e96021164f9f7b493279a4ac7fb
slowly <slowly@live.ru>
2025-09-17 15:54:48 +0300

    KS-2-4 move known variables out from constants, changed the percentage variable for more flexible operation

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_2
 
-const val crystalOre = 7
-const val ironOre = 11
-const val materialsBuff = 0.2
-
 fun main() {
 
-    val bonusCrystalOre = (crystalOre * materialsBuff).toInt()
-    val bonusIronOre = (ironOre * materialsBuff).toInt()
+    val crystalOre = 7
+    val ironOre = 11
+    val materialsBuff = 20
+
+    val bonusCrystalOre = (crystalOre * (materialsBuff / 100.0)).toInt()
+    val bonusIronOre = (ironOre * (materialsBuff / 100.0)).toInt()
 
     println("You received $bonusCrystalOre bonus crystal ore(s) by buff")
     println("You received $bonusIronOre bonus iron ore(s) by buff")
