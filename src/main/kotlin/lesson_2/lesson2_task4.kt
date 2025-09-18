2eab7bd07e55be7eb9c5520d7ee20b7dc9d8e08c
slowly <slowly@live.ru>
2025-09-18 16:41:19 +0300

    KS-2-4 move percent converter variable to constant

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,15 @@
 package org.example.lesson_2
 
+const val PERCENT_CONVERTER = 100.0
+
 fun main() {
 
     val crystalOre = 7
     val ironOre = 11
     val materialsBuff = 20
 
-    val bonusCrystalOre = (crystalOre * (materialsBuff / 100.0)).toInt()
-    val bonusIronOre = (ironOre * (materialsBuff / 100.0)).toInt()
+    val bonusCrystalOre = (crystalOre * (materialsBuff / PERCENT_CONVERTER)).toInt()
+    val bonusIronOre = (ironOre * (materialsBuff / PERCENT_CONVERTER)).toInt()
 
     println("You received $bonusCrystalOre bonus crystal ore(s) by buff")
     println("You received $bonusIronOre bonus iron ore(s) by buff")
