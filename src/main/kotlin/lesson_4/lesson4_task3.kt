4e3a89b0113a567485a22a11114feafa269c8c8b
slowly <slowly@live.ru>
2025-09-21 20:04:18 +0300

    KS-4-4 refactoring

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_4
 
+const val IS_SUN: Boolean = true
+const val IS_AWNING_OPEN: Boolean = true
+const val AIR_HUMIDITY: Int = 20
+const val SEASON_OF_YEAR: String = "Зима"
+
 fun main() {
 
-    val isSun: Boolean = true
-    val isAwningOpen: Boolean = true
-    val airHumidity: Int = 20
-    val seasonOfYear: String = "Зима"
+    val favorableConditions = IS_SUN && IS_AWNING_OPEN && AIR_HUMIDITY == 20 && SEASON_OF_YEAR != "Зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${isSun && isAwningOpen && airHumidity == 20 && seasonOfYear != "Зима"}")
+    println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 
 }
