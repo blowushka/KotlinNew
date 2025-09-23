270d1f19f6d079d48681b0bc5dfe03f12f3358d0
slowly <slowly@live.ru>
2025-09-24 02:50:40 +0300

    KS-4-5 refactoring

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -5,6 +5,7 @@ const val CREW_COUNT_MIN = 55
 const val CREW_COUNT_MAX = 70
 const val PROVISION_COUNT_MIN = 50
 const val IS_FAVORABLE_WEATHER = true
+const val MIN_AMOUNT_OF_CREW_WHEN_SHIP_IS_DAMAGED = 70
 
 fun main() {
 
@@ -21,7 +22,7 @@ fun main() {
     val isFavorableWeather = readln().toBoolean()
 
     val shipIsReady =
-        (isDamaged != IS_DAMAGED && (crewCount >= CREW_COUNT_MIN && crewCount <= CREW_COUNT_MAX) && amountOfProvisions > PROVISION_COUNT_MIN && isFavorableWeather == IS_FAVORABLE_WEATHER) || (isDamaged == IS_DAMAGED && crewCount == 70 && amountOfProvisions >= 50 && isFavorableWeather == IS_FAVORABLE_WEATHER)
+        (isDamaged != IS_DAMAGED && (crewCount >= CREW_COUNT_MIN && crewCount <= CREW_COUNT_MAX) && amountOfProvisions > PROVISION_COUNT_MIN && isFavorableWeather == IS_FAVORABLE_WEATHER) || (isDamaged == IS_DAMAGED && crewCount == MIN_AMOUNT_OF_CREW_WHEN_SHIP_IS_DAMAGED && amountOfProvisions >= PROVISION_COUNT_MIN && isFavorableWeather == IS_FAVORABLE_WEATHER)
 
     println("Is the ship ready to sail?: $shipIsReady")
 
